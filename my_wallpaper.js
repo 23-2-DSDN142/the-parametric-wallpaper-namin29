2079660976e2fb34e748e5952e5961d819b66c5a
namin29 <140354195+namin29@users.noreply.github.com>
2023-08-07 04:10:49 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,82 +1,103 @@
-//your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
-let cellheight = 300;
-let cellwidth = 300;
-let eyesize = 30; // 30
-let nosesize = 20;
-
+let cellheight = 300; // 300
+let cellwidth = 300; // 300
+let eyesize = 28; // 30
+let nosesize = 18; // 20
+let bonecolor = [255, 255, 255]; // [255, 255, 255]
+let backgroundcolor = [220, 214, 247]; // [0, 0, 0,]
+let accentcolor = [166, 177, 225]; // [150, 50, 50]
+let bonetipsize = 10; // 12
+let eyeHeight = 115
+let noseHeight = 135
+let boneHeight = 120
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.resolution(NINE_LANDSCAPE);
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
-  pWallpaper.grid_settings.cell_width  = cellwidth;
+  pWallpaper.grid_settings.cell_width = cellwidth;
   pWallpaper.grid_settings.cell_height = cellheight;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset = 80;
 }
 
 
 function wallpaper_background() {
   clear();
-  background(0, 0, 0); //light honeydew green colour
+  background(backgroundcolor);
 }
 
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  //rect(40 ,40, rect_width, rect_height);
 
-  
   noStroke();
-  fill(255,255,255);
+  fill(accentcolor)
+  circle(cellwidth / 2, boneHeight / 0.7, 120, 120)
+
+  fill(bonecolor);
   circle(cellwidth / 2, 100, 120);
-  rect(cellwidth / 2 -35, cellheight / 2 -50, 70, 75);
-  triangle(cellwidth / 2 -35, cellheight / 2 +25, cellwidth / 2 +35, cellheight / 2 +25, cellwidth / 2,cellheight / 2 +35);
+  rect(cellwidth / 2 - 35, cellheight / 2 - 50, 70, 75);
+  triangle(cellwidth / 2 - 35, cellheight / 2 + 25, cellwidth / 2 + 35, cellheight / 2 + 25, cellwidth / 2, cellheight / 2 + 35);
 
-  fill(0,0,0);
-  rect(cellwidth / 2 +50, 0, 20, 150);
-  rect(cellwidth / 2 -50, 0, -20, 150); // black out the sides
+  fill(backgroundcolor);
+  rect(cellwidth / 2 + 50, 40, 10, 100);
+  rect(cellwidth / 2 - 50, 40, -10, 100); // black out the sides
 
 
- // if () {
- //   rect(cellwidth / 2 - 50, cellheight / 2 + 10, 100, 50);
- // } else {
- //   rect(cellwidth / 2 - 50, cellheight / 2 + 10, 100, 5);
- // }
+  // if () {
+  //   rect(cellwidth / 2 - 50, cellheight / 2 + 10, 100, 50);
+  // } else {
+  //   rect(cellwidth / 2 - 50, cellheight / 2 + 10, 100, 5);
+  // }
 
-  drawEyes(150, 110);
-  drawNose(150, 130);
-  drawbones(150, 150);
+  drawEyes(150, eyeHeight);
+  drawNose(150, noseHeight);
+  drawbones(150, boneHeight);
 }
 
-function drawEyes (x, y) {
+function drawEyes(x, y) {
 
-  fill(0,0,0);
-  circle(x -28, y, eyesize);
-  circle(x +28, y, eyesize); // larger circles
+  fill(0, 0, 0);
+  circle(x - 28, y, eyesize);
+  circle(x + 28, y, eyesize); // larger circles
 
-  circle(x -18, y -8, eyesize -15); // smaller circles
-  circle(x +18, y -8, eyesize -15);
+  circle(x - 18, y - 8, eyesize - 15); // smaller circles
+  circle(x + 18, y - 8, eyesize - 15);
 
-  fill(200, 0, 0);
-  circle(x -25, y -2, eyesize -20); // red dots
-  circle(x +25, y -2, eyesize -20);
-  
+  fill(accentcolor);
+  circle(x - 27, y, eyesize - 10); // eyeball whites
+  circle(x + 27, y, eyesize - 10);
+
+  fill(0, 0, 0)
+  circle(x - 29, y - 1, eyesize - 18); // pupil
+  circle(x + 28, y + 1, eyesize - 18);
 }
 
-function drawNose (x, y) {
+function drawNose(x, y) {
 
-  fill(0,0,0);
+  fill(0, 0, 0);
   circle(x, y, nosesize);
-  circle(x, y -10, nosesize -10);
+  circle(x, y - 10, nosesize - 10);
 }
 
-function drawbones (x, y) {
+function drawbones(x, y) {
 
-  stroke(255,255,255);
-  strokeWeight(10);
-  line(cellwidth / 2 - 80, cellheight / 2 + 40, cellwidth / 2 + 80, cellheight / 2 + 120); // horizontal bone
-  line(cellwidth / 2 - 10, cellheight / 2 - 60, 20, 120); // vertical bone
+  stroke(bonecolor);
+  strokeWeight(bonetipsize / 1.5);
+  line(x - 80, y + 40, x + 80, y + 120);
+  line(x - 80, y + 120, x + 80, y + 40);
+
+  noStroke()
+  fill(bonecolor)
+  circle(x - 82, y + 44, bonetipsize, bonetipsize);
+  circle(x - 78, y + 36, bonetipsize * 0.9, bonetipsize * 0.9);
+
+  circle(x - 82, y + 116, bonetipsize, bonetipsize);
+  circle(x - 78, y + 124, bonetipsize * 0.9, bonetipsize * 0.9);
+
+  circle(x + 82, y + 116, bonetipsize, bonetipsize);
+  circle(x + 78, y + 124, bonetipsize * 0.9, bonetipsize * 0.9);
+
+  circle(x + 82, y + 44, bonetipsize, bonetipsize);
+  circle(x + 78, y + 36, bonetipsize * 0.9, bonetipsize * 0.9);
 }
